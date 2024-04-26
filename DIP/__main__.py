5ed69a2dad1e42140c4d16226de56dc430c7764e
Moosems <95927277+Moosems@users.noreply.github.com>
2024-04-26 08:12:34 -0400

    Quick additions

--- DIP/__main__.py
+++ DIP/__main__.py
@@ -1,8 +1,30 @@
-from tukaan import App, MainWindow
+from tukaan import App, MainWindow, KolorScheme, Button
+from tukaan._tcl import Tcl
+from platform import system
 
 
 def main(no_loop: bool = False) -> None:
+    """Main function for the DIP Code Editor"""
     root = App(name="DIP")
     window = MainWindow(title="DIP")  # noqa: F841
+
+
+    # Get clam theme
+    if not system() in {"Darwin", "Windows"}:
+        root.theme = KolorScheme # type: ignore
+
+
+    # TODO: remove placeholder widget
+    button = Button(window, text="Click Me!", tooltip="Placeholder Widget ;)")
+    button.grid()
+
+
+    # Resize Window
+    width = Tcl.call(int, "winfo", "reqwidth", button)
+    height = Tcl.call(int, "winfo", "reqheight", button)
+    window.width = width
+    window.height = height
+
+
     if not no_loop:
         root.run()
